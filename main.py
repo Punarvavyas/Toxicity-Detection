c18599186cba10ca40d7ab21d640dca1e2c8b814
Sahil Fruitwala <sh941551@dal.ca>
2020-03-20 21:09:23 -0300

    Deployment with detection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+from prediction import cleanandtransform
+
 
 app = Flask(__name__)
 
@@ -7,11 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template('search.html')
 
+
 @app.route('/report/', methods=['GET'])
 def report():
-    searchText = request.args.get('searchText')
-    print(searchText)
-    return render_template('report.html', text=searchText)
+    searchText = request.args.get('searchText').encode('ascii', 'ignore').decode('ascii')
+    prediction = cleanandtransform.clean_typed_data(str(searchText))
+    return render_template('report.html', text=prediction)
+
 
 @app.route('/analysis')
 def analysis():
@@ -20,4 +24,4 @@ def analysis():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000, debug=True)
+    app.run(port=5000, debug=True)
